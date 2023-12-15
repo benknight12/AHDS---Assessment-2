6aee4d8d94fa6449dc0372fbb11f6840ede7e328
benknight12 <benknight28@gmail.com>
2023-12-15 14:45:38 +0000

    First commit to git with some basic data cleaning file, mainly doing this to initialise the git repository

---
+++ ahds-summative-data-prep.R
@@ -0,0 +1,26 @@
+
+## have a look at the nhanes diet, bmi and demographic data, and save to csv
+## data from here - https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=Examination&CycleBeginYear=2005
+## https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=Demographics&CycleBeginYear=2005
+## https://wwwn.cdc.gov/nchs/nhanes/search/datapage.aspx?Component=Dietary&Cycle=2005-2006
+
+## diet questionnaire
+library(foreign)
+diet <- read.csv("raw/FFQRAW_D.csv")
+head(diet)
+write.csv(diet, "clean/FFQRAW_D.csv", row.names=FALSE)
+
+## bmi
+bmi <- read.csv("raw/BMI.csv")
+head(bmi)
+write.csv(bmi, "clean/BMX_D.csv", row.names=FALSE)
+
+# save id and bmi only from BMXBMI data
+bmi = bmi[, c('SEQN', 'BMXBMI')]
+write.csv(bmi, 'clean/BMI.csv', row.names=FALSE)
+
+
+## demographics
+demo <- read.csv("raw/DEMO_D.csv")
+head(demo)
+write.csv(demo, "clean/DEMO_D.csv", row.names=FALSE)
